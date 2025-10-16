95b9c1d18ed455c0778d4f0b2d56530be89855c1
thotska <thotska@gmail.com>
2025-10-15 20:42:15 -0500

    updated end to end test

--- tests/userAPIs.spec.ts
+++ tests/userAPIs.spec.ts
@@ -1,50 +1,40 @@
 import { test, expect, request } from '@playwright/test';
 import z from 'zod';
 import { faker } from '@faker-js/faker';
-import { getAPI, postAPI, putAPI, deleteAPI} from '../utils/apiCallHelper';
+import { getAPI, postAPI, putAPI, deleteAPI } from '../utils/apiCallHelper';
 
-
-test.describe('User API Tests', () => {
+test("end to end test - Create, Get, Update, Delete user", async ({ request }) => {
     const BASE_URL = `${process.env.BASE_URL}${process.env.API_VERSION}`
     const createNewUserRequestBody = {
-            "id": 12452,
-            "username": "TestUserNameTetiana99",
-            "firstName": faker.person.firstName(),
-            "lastName": faker.person.lastName(),
-            "email": faker.internet.email(),
-            "password": faker.internet.password(),
-            "phone": faker.phone.number(),
-            "userStatus": 0
-        };
+        "id": 12452,
+        "username": "TestUserNameTetiana99",
+        "firstName": faker.person.firstName(),
+        "lastName": faker.person.lastName(),
+        "email": faker.internet.email(),
+        "password": faker.internet.password(),
+        "phone": faker.phone.number(),
+        "userStatus": 0
+    };
 
-        const createNewUserResponseSchema = z.object({
-            code: z.literal(200),
-            type: z.literal('unknown'),
-            message: z.literal(createNewUserRequestBody.id.toString()),
-        });
-    test('Create a new User', async ({ request }) => {
-        const createNewUserResponse = await request.post(`${BASE_URL}/user`, {
-            data: createNewUserRequestBody
-        });
-        
-        await postAPI(
-            request,
-            `${BASE_URL}/user`,
-            createNewUserRequestBody,
-            200,
-            createNewUserResponseSchema);
+    const createNewUserResponseSchema = z.object({
+        code: z.literal(200),
+        type: z.literal('unknown'),
+        message: z.literal(createNewUserRequestBody.id.toString()),
     });
+    await postAPI(
+        request,
+        `${BASE_URL}/user`,
+        createNewUserRequestBody,
+        200,
+        createNewUserResponseSchema);
 
-    test("Get user by username", async ({request}) => {
-        const username = createNewUserRequestBody.username;
-        
         await getAPI(
             request,
-            `${BASE_URL}/user/${username}`,
+            `${BASE_URL}/user/${createNewUserRequestBody.username}`,
             200,
             z.object({
                 id: z.number().int(),
-                username: z.literal(username),
+                username: z.literal(createNewUserRequestBody.username),
                 firstName: z.string(),
                 lastName: z.string(),
                 email: z.string().email(),
@@ -53,60 +43,34 @@ test.describe('User API Tests', () => {
                 userStatus: z.number().int()
             })
         );
-    });
-
-
-    test("Update user by username", async ({request}) => {
-        const username = createNewUserRequestBody.username;
-        const updateUserRequestBody = {
-            "id": 12452,
-            "username": username,
-            "firstName": faker.person.firstName(),
-            "lastName": faker.person.lastName(),
-            "email": faker.internet.email(),
-            "password": faker.internet.password(),
-            "phone": faker.phone.number(),
-            "userStatus": 1
-        };
-
-        const updateUserResponseSchema = z.object({
-            code: z.literal(200),
-            type: z.literal('unknown'),
-            message: z.literal(updateUserRequestBody.id.toString()),
-        });
-
         await putAPI(
             request,
-            `${BASE_URL}/user/${username}`,
-            updateUserRequestBody,
-            200,
-            updateUserResponseSchema
-        );
-    });
-
-
-
-
-
-
-
-
-
-            
-    test("Delete user by username", async ({request}) => {
-        const username = createNewUserRequestBody.username;
-
-        await deleteAPI(
-            request,
-            `${BASE_URL}/user/${username}`,
+            `${BASE_URL}/user/${createNewUserRequestBody.username}`,
+            {
+                "id": 12452,
+                "username": createNewUserRequestBody.username,
+                "firstName": faker.person.firstName(),
+                "lastName": faker.person.lastName(),
+                "email": faker.internet.email(),
+                "password": faker.internet.password(),
+                "phone": faker.phone.number(),
+                "userStatus": 1
+            },
             200,
             z.object({
                 code: z.literal(200),
                 type: z.literal('unknown'),
-                message: z.literal(username),
+                message: z.literal("12452"),
+            })
+        );
+        await deleteAPI(
+            request,
+            `${BASE_URL}/user/${createNewUserRequestBody.username}`,
+            200,
+            z.object({
+                code: z.literal(200),
+                type: z.literal('unknown'),
+                message: z.literal(createNewUserRequestBody.username),
             })
         );
     });
-});
-
-
